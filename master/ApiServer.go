19f0a271c9750bef16ec63f217c6ece69e34fa1f
kun <412657308@qq.com>
2018-11-18 10:20:31 +0800

    Init

---
+++ master/ApiServer.go
@@ -0,0 +1,52 @@
+package master
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type ApiServer struct {
+	httpServer *http.Server
+}
+
+var (
+	G_apiServer *ApiServer
+)
+
+func handleJobSaver(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func InitServer() (err error) {
+	var (
+		mux        *http.ServeMux
+		listener   net.Listener
+		httpServer *http.Server
+	)
+	// 配置路由
+	mux = http.NewServeMux()
+	mux.HandleFunc("/job/save", handleJobSaver)
+
+	// 启动TCP监听
+	if listener, err = net.Listen("tcp", ":"+strconv.Itoa(G_config.ApiPort)); err != nil {
+		return
+	}
+
+	// 创建http服务
+	httpServer = &http.Server{
+		ReadTimeout:  time.Duration(G_config.ApiReadTimeout) * time.Millisecond,
+		WriteTimeout: time.Duration(G_config.ApiWriteTimeout) * time.Millisecond,
+		Handler:      mux,
+	}
+
+	G_apiServer = &ApiServer{
+		httpServer: httpServer,
+	}
+
+	//启动服务端
+	go httpServer.Serve(listener)
+
+	return
+}
---
+++ master/main/master.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/fzkun/crontab/master"
+	"runtime"
+)
+
+var (
+	confFile string //配置文件路径
+)
+
+func initArgs() {
+	// master -config ./master.json
+	// master -h
+	flag.StringVar(&confFile, "config", "./master.json", "指定master.json")
+	flag.Parse()
+
+}
+
+func initEvn() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+
+	var (
+		err error
+	)
+
+	initArgs()
+
+	//初始化线程
+	initEvn()
+
+	//加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 启动api http服务
+	if err = master.InitServer(); err != nil {
+		goto ERR
+	}
+
+	//正常退出
+	return
+
+ERR:
+	fmt.Println(err)
+}
